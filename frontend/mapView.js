56c5fb86323ee3b7234c7ff9f50e44b7075e5b0c
Kevin <kevinmoore9@users.noreply.github.com>
2017-05-09 15:25:04 -0700

    add MapView and follow location

--- frontend/mapView.js
+++ frontend/mapView.js
@@ -6,73 +6,79 @@ import {
   View
 } from 'react-native';
 
+import Map from 'react-native-maps';
+
+
 class MapView extends Component {
   constructor() {
     super();
     this.state = {
-      initialPosition: 'unknown',
-      lastPosition: 'unknown',
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
     };
+    this.onRegionChange = this.onRegionChange.bind(this);
 
   }
 
-watchID: ?number = null;
+  // watchID: ?number = null;
 
 
-componentDidMount() {
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      var initialPosition = JSON.stringify(position);
-      this.setState({initialPosition});
-    },
-    (error) => alert(JSON.stringify(error)),
-    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-  );
-  this.watchID = navigator.geolocation.watchPosition((position) => {
-    var lastPosition = JSON.stringify(position);
-    this.setState({lastPosition});
-  });
-}
+  // componentDidMount() {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       var initialPosition = JSON.stringify(position);
+  //       this.setState({initialPosition});
+  //     },
+  //     (error) => alert(JSON.stringify(error)),
+  //     {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+  //   );
+  //   this.watchID = navigator.geolocation.watchPosition((position) => {
+  //     var lastPosition = JSON.stringify(position);
+  //     this.setState({lastPosition});
+  //   });
+  // }
+  //
+  // componentWillUnmount() {
+  //   navigator.geolocation.clearWatch(this.watchID);
+  // }
 
-componentWillUnmount() {
-  navigator.geolocation.clearWatch(this.watchID);
-}
+  onRegionChange(region) {
+    this.setState({ region });
+    console.log(region);
+  }
 
   render() {
-
-    return (
-      <View style={styles.container}>
-        <Text>
-          <Text style={styles.title}>Initial position: </Text>
-          {this.state.initialPosition}
-        </Text>
-        <Text>
-          <Text style={styles.title}>Current position: </Text>
-          {this.state.lastPosition}
-        </Text>
-      </View>
-    );
+      return (
+        <Map style={styles.map}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+          showsUserLocation={true}
+          followsUserLocation={true}
+          />
+      );
   }
 }
-
+//
+// <View style={styles.container}>
+//   <Text>
+//     <Text style={styles.title}>Initial position: </Text>
+//     {this.state.initialPosition}
+//   </Text>
+//   <Text>
+//     <Text style={styles.title}>Current position: </Text>
+//     {this.state.lastPosition}
+//   </Text>
+// </View>
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  map: {
+    top: 0, left: 0, right: 0, bottom: 0, position: 'absolute'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
 module.exports = MapView;
